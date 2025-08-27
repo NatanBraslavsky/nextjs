9d4f2805dbd091c0d18d6928fa7e75564ff551aa
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-08-27 18:05:45 -0300

    saldo bold paragrafo

--- projetos/blog-next/src/app/components/transacao/Saldo.tsx
+++ projetos/blog-next/src/app/components/transacao/Saldo.tsx
@@ -6,7 +6,7 @@ export interface SaldoProps{
 export default function Saldo(props: SaldoProps){
     return(
         <div className=" border border-zinc-300 rounded-lg flex flex-col gap-0.5 px-3 py-2 text-[20px]">
-            <p>{props.nome} R$</p>
+            <p className="font-bold">{props.nome} R$</p>
             <p>{props.valor}</p>
         </div>
     )
