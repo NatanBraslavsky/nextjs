5a9d37674cdb3b04d897d69d8d0e534e97353853
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-08-28 15:49:28 -0300

    adição dos conteudos da tabela para teste

--- projetos/blog-next/src/app/components/ui/info/Tabela.tsx
+++ projetos/blog-next/src/app/components/ui/info/Tabela.tsx
@@ -1,10 +1,12 @@
+import { IoTrashOutline } from "react-icons/io5";
+
 interface ThProps{
     children:string
 }
 
 function Th({children}: ThProps){
     return(
-        <th className="text-start text-2xl border-b border-b-white">{children}</th>
+        <th className="text-start text-[18px] sm:text-2xl pb-5">{children}</th>
     )
 }
 
@@ -19,11 +21,17 @@ export default function Tabela(){
                         <Th>Tipo</Th> 
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="text-[14px] sm:text-[1.1rem] text-zinc-400">
+                    <td className="pb-2">Salario</td>
+                    <td className="pb-2">R$1500</td>
+                    <td className="pb-2 pl-2 sm:pl-3">✅</td>
+                    <td className="pb-2"><IoTrashOutline className="hover:cursor-pointer w-5 h-5 sm:w-7 sm:h-7"/></td>
+                </tbody>
+                <tbody className="text-[14px] sm:text-[1.1rem] text-zinc-400">
                     <td>Salario</td>
-                    <td>1500</td>
-                    <td>✅</td>
-                    <td></td>
+                    <td>R$1500</td>
+                    <td className="pl-2 sm:pl-3">❌</td>
+                    <td ><IoTrashOutline className="hover:cursor-pointer w-5 h-5 sm:w-7 sm:h-7"/></td>
                 </tbody>
             </table>
         </div>
--- projetos/blog-next/src/app/components/ui/transacao/Saldo.tsx
+++ projetos/blog-next/src/app/components/ui/transacao/Saldo.tsx
@@ -5,7 +5,7 @@ export interface SaldoProps{
 
 export default function Saldo(props: SaldoProps){
     return(
-        <div className="border rounded-lg flex flex-col gap-0.5 px-3 py-2 text-[20px] max-sm:py-1">
+        <div className="border border-zinc-600 rounded-lg flex flex-col gap-0.5 px-3 py-2 text-[20px] max-sm:py-1">
             <p className="font-bold max-sm:text-[18px]">{props.nome} R$</p>
             <p className="max-sm:text-[14px]">{props.valor}</p>
         </div>
